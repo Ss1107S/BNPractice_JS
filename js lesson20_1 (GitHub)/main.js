08601ea99227a507ff291a9d88f44046c2a71b9e
Svetlana <152962158+Ss1107S@users.noreply.github.com>
2025-08-01 19:31:14 +0200

    Lesson 20_1
    Lesson 20_1-->Lesson 20--> Lesson 19_1-->Lesson 19
    Without folder with note_modules (!)
    
    Data Tables and Databases
    With used Tailwind (tailwindcss.com flowbite.com), Node (nodejs.org)
    
    // Two tabs, 1 for Guests, 1 for Events. Each of the tabs should have its own DataTable, it must become visible when its tab is active,
    // do not reinitiate the DataTable, just change its visibility.
    // Guests should have 2 columns, 1 for ID, a second for Guest Name.
    // Events should have 4 entries, 1 for ID, 2 for Guest List - (list of all guest names or ids), 3 is Schedule Date, and 4th one is random hash or number.
    // Both of the datatables, need data adders, for Guests, you need input for new user name,
    // ID should be automatically incremented. For Events, you need input for adding tags and the tags must exist in Guests datatable, for Schedule Date you need a date picker input.
    // And when you submit any of these data adders, the data is processed from the inputs/entries and added to the datatable.
    
    // add a click event for the dropdown:
    /* 
     on the click event:
       - use the contacts.guestnames to display all of the names inside <ul> 
         and the names need to be also having another click event, which will close the dropdown and add the clicked name into another array
         (and this array is a temporary array (that needs to be cleared when event is added), lets call this array entered_guestlist), and 
         you add a tag element above the dropdown button (instead of the 'Default' text, you put the guest name that clicked) - the newly added tag element
         needs to have a click event for removal and this event is on a remove icon, which will remove the tag and will also remove it from the temporary array.  
    */

---
+++ js lesson20_1 (GitHub)/main.js
@@ -0,0 +1,24 @@
+if (document.getElementById("guests-table") && typeof simpleDatatables.DataTable !== 'undefined') {
+    const dataTable = new simpleDatatables.DataTable("#guests-table", {
+        searchable: false,
+        perPageSelect: false
+    });
+}
+if (document.getElementById("events-table") && typeof simpleDatatables.DataTable !== 'undefined') {
+    const dataTable = new simpleDatatables.DataTable("#events-table", {
+        searchable: false,
+        perPageSelect: false
+    });
+}
+document.getElementById('new_guest_name').addEventListener('click', () => {
+    document.body.appendChild(newElm1);
+});
+
+
+document.getElementById('id').addEventListener('click', () => {
+    toNewId();
+});
+
+//toNewId(){
+//    this.index = (this.index + 1);
+//};
